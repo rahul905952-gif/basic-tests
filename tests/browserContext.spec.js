28374d160e6fbc724b8d9d6772bd19404e09f557
Rahul <rahul905952@gmail.com>
2026-07-26 20:10:16 +0530

    added explicit wait for specific test

--- tests/browserContext.spec.js
+++ tests/browserContext.spec.js
@@ -26,6 +26,7 @@ test('browser context', async () => {
 
 
 test('new page handle in context', async () => {
+    test.setTimeout(60000);
     const browser = await chromium.launch();
     const context = await browser.newContext();
     const page = await context.newPage();
