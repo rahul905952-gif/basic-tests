4c0004181fd92af76f0d5ec0e315625145f0339b
Rahul <rahul905952@gmail.com>
2026-07-26 19:58:17 +0530

    added assertion with domcontentloaded

--- tests/browserContext.spec.js
+++ tests/browserContext.spec.js
@@ -1,26 +1,26 @@
-const {test, expect, chromium} = require('@playwright/test');
+const { test, expect, chromium } = require('@playwright/test');
 
-test('browser context', async()=>{
+test('browser context', async () => {
 
-const browser = await chromium.launch();
-const context1 = await browser.newContext();
-const context2 = await browser.newContext();
+    const browser = await chromium.launch();
+    const context1 = await browser.newContext();
+    const context2 = await browser.newContext();
 
-const page1 = await context1.newPage();
-const page2 = await context2.newPage();
+    const page1 = await context1.newPage();
+    const page2 = await context2.newPage();
 
-await page1.goto("https://www.saucedemo.com/");
-await page1.fill("#user-name", "standard_user");
-await page1.fill("#password", "secret_sauce");
-await page1.click("#login-button");
-await expect(page1).toHaveTitle('Swag Labs');
+    await page1.goto("https://www.saucedemo.com/");
+    await page1.fill("#user-name", "standard_user");
+    await page1.fill("#password", "secret_sauce");
+    await page1.click("#login-button");
+    await expect(page1).toHaveTitle('Swag Labs');
 
 
-await page2.goto("https://www.saucedemo.com/");
-await page2.fill("#user-name", "locked_out_user");
-await page2.fill("#password", "secret_sauce");
-await page2.click("#login-button");
-await expect(page2).toHaveTitle('Swag Labs');
+    await page2.goto("https://www.saucedemo.com/");
+    await page2.fill("#user-name", "locked_out_user");
+    await page2.fill("#password", "secret_sauce");
+    await page2.click("#login-button");
+    await expect(page2).toHaveTitle('Swag Labs');
 
 });
 
@@ -29,7 +29,7 @@ test('new page handle in context', async () => {
     const browser = await chromium.launch();
     const context = await browser.newContext();
     const page = await context.newPage();
-   // const page2 = await context.newPage();
+    // const page2 = await context.newPage();
     await page.goto("https://testautomationpractice.blogspot.com/");
     //await page2.goto("https://www.saucedemo.com/");
 
@@ -39,12 +39,16 @@ test('new page handle in context', async () => {
         page.getByText("merrymoonmary").click()
     ]);
 
-   await newPage.waitForLoadState('domcontentloaded');
-   // console.log("New page URL:", newPage.url());
+    await newPage.waitForLoadState('domcontentloaded');
+
+    // Verify page title
+    await expect(newPage).toHaveTitle(/merrymoonmary Stock Image/);
+    //await newPage.waitForLoadState();
+    // console.log("New page URL:", newPage.url());
     await browser.close();
 });
-    
-test('download', async({page})=>{
+
+test('download', async ({ page }) => {
     await page.goto("https://testautomationpractice.blogspot.com/p/download-files_25.html");
     await page.getByLabel('Enter Text:').fill('test');
     await page.locator('#generateTxt').click();
@@ -53,6 +57,6 @@ test('download', async({page})=>{
         page.waitForEvent('download'), page.locator('#txtDownloadLink').click()
     ]);
 });
-  
+
 
 
